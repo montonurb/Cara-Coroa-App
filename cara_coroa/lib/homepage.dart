42711691732f4f1a3985b368a884a6b37d966899
Bruno Monteiro <brunomoncar007@gmail.com>
2020-06-22 19:19:41 -0300

    tela para dar play

--- cara_coroa/lib/homepage.dart
+++ cara_coroa/lib/homepage.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:cara_coroa/resultadoscreen.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -6,10 +9,29 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  void _iniciar(){
+    var escolhas = ["cara", "coroa"];
+    var numero = Random().nextInt(escolhas.length);
+    var resultado = escolhas[numero];
+    Navigator.push(context, MaterialPageRoute(builder: (context) => ResultadoScreen(resultado)));
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xff61bd86),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset("images/logo.png"),
+            GestureDetector(
+              onTap: _iniciar,
+              child: Image.asset("images/botao_jogar.png"),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
